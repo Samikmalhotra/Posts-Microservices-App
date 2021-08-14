533d4043f2b405f5896d481d40d1aad4f203d1d0
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-14 10:10:22 +0530

    initiated comments as a service

--- comments/index.js
+++ comments/index.js
@@ -1,16 +1,29 @@
 import express from 'express'
 import cors from 'cors'
-
+import {v4 as uuidv4} from 'uuid'
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+const commentsByPostId = {};
 
+app.get('/posts/:id/comments', (req, res) => {
+  res.json(commentsByPostId[req.params.id] || []);
+})
+app.post('/posts/:id/comments', (req, res) => {
+ const commentId = uuidv4();
+ const {content} = req.body;
+
+ const comments = commentsByPostId[req.params.id] || [];
+
+ comments.push({id: commentId, content});
+
+ commentsByPostId[req.params.id] = comments;
+
+ res.status(201).json(comments);
+})
 app.listen(4001, () => {
   console.log('Comments listening on port 4001!');
 })
