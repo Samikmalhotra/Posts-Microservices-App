73bf80fc6e9a0fcef6f27d05dce7b8f7ced9904a
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-17 17:32:54 +0530

    updated moderation status

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,22 +1,21 @@
-import express from 'express'
+import express from 'express';
 import axios from 'axios';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/events', async(req,res)=>{
-    const event = req.body;
-    console.log(event);
-    await axios.post('http://localhost:4000/events', event)
-    await axios.post('http://localhost:4001/events', event)
-    await axios.post('http://localhost:4002/events', event)
+app.post('/events', async (req, res) => {
+  const event = req.body;
+  console.log(event);
+  await axios.post('http://localhost:4000/events', event);
+  await axios.post('http://localhost:4001/events', event);
+  await axios.post('http://localhost:4002/events', event);
+  await axios.post('http://localhost:4003/events', event);
 
-    res.json({status: 'OK'});
-})
-
-
+  res.json({ status: 'OK' });
+});
 
 app.listen(4005, () => {
-    console.log('Event Bus is listening on port 4005!');
-})
+  console.log('Event Bus is listening on port 4005!');
+});
--- moderation/index.js
+++ moderation/index.js
@@ -1,14 +1,28 @@
-import express from 'express'
-import axios from 'axios'
+import express from 'express';
+import axios from 'axios';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/events',(req,res)=>{
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
 
-})
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
 
-app.listen(4003, ()=>{
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+});
+
+app.listen(4003, () => {
   console.log('Moderation listening on 4003');
-})
+});
